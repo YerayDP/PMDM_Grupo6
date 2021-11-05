ba204fcf4a9c1f4e8bee8013dde3cc663d015f9b
YerayDP <yerayd99@gmail.com>
2021-11-05 09:37:54 +0100

    Admin List

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { LoadingController } from '@ionic/angular';
-=======
 import { ViewChild } from '@angular/core';
 import { IonList } from '@ionic/angular';
->>>>>>> 8c4c89236997b48c7ed266273381f711fac2329d
 import { RestService } from 'src/app/services/rest.service';
+import { LoadingController } from '@ionic/angular';  
 
 @Component({
   selector: 'app-tab1',
