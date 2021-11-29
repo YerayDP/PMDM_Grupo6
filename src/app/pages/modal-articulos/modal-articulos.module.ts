a7c4eefa33d5ef40e221c71b4b344c0fdce519eb
YerayDP <yerayd99@gmail.com>
2021-11-29 11:49:41 +0100

    Pull f

---
+++ src/app/pages/modal-articulos/modal-articulos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ModalArticulosPageRoutingModule } from './modal-articulos-routing.module';
+
+import { ModalArticulosPage } from './modal-articulos.page';
+import { ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ModalArticulosPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [ModalArticulosPage]
+})
+export class ModalArticulosPageModule {}
---
+++ src/app/pages/modal-articulos/modal-articulos.page.ts
@@ -0,0 +1,137 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IonSearchbar,LoadingController, ModalController } from '@ionic/angular';
+import { RestService } from 'src/app/services/rest.service';
+import { ModalPrecioPage } from '../modal-precio/modal-precio.page';
+
+@Component({
+  selector: 'app-modal-articulos',
+  templateUrl: './modal-articulos.page.html',
+  styleUrls: ['./modal-articulos.page.scss'],
+})
+export class ModalArticulosPage implements OnInit {
+
+  @Input() p : any;
+
+  @ViewChild('search', {static:false}) search: IonSearchbar;
+  items2:any[] = [];
+  items:any
+  articulos: any = [];
+  searchField: FormControl;
+  articulosFin: any = [];
+
+  constructor(private modalCtrl: ModalController, private restService: RestService, private loadingCtrl: LoadingController) { }
+
+  ngOnInit() {
+
+    this.rellenar();
+
+    this.showLoading();
+  }
+  ionViewDidEnter(){
+    setTimeout(() =>{
+      this.search.setFocus();
+    })
+  }
+
+  getItems(ev:any){
+
+    
+    const val = ev.target.value;
+    if (val && val.trim()!='') {
+      this.articulosFin = this.items.filter((item : any)=>{
+        console.log(item.description)
+        return (item.description.toLowerCase().indexOf(val.toLowerCase())>-1);
+      })
+    }
+  }
+
+  obtenerArticulos()
+  {
+  this.restService.obtenerArticulos(this.restService.token)
+    .then(data => {
+      this.articulos = data;
+      this.items=this.articulos.data;
+      this.items2=this.items;
+  });
+}
+
+  salirSinArgumentos()
+  {
+    this.modalCtrl.dismiss();
+  }
+
+  salirConArgumentos()
+  {
+    console.log('Atras');
+    this.modalCtrl.dismiss({
+    });
+  }
+
+  async abrirmodalArticulos(producto:any)
+  {
+   const modal = await this.modalCtrl.create({
+     component: ModalPrecioPage,
+     componentProps: {
+       producto
+     }
+   });
+
+   await modal.present();
+
+   const { data } = await modal.onDidDismiss();
+   console.log(data);
+
+  }
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({  
+    message: 'Loading.....'
+    });  
+    loading.present();
+    this.obtenerArticulos();
+    setTimeout(() => {
+      loading.dismiss();
+    }, 500 );
+    
+
+ }
+
+ rellenar()
+ {
+   var articulos2 :any[] = [];
+   var productos_id: any[] =[];
+   
+   for(let i = 0; i<this.p.length;i++)
+   {
+    var id = this.p[i].article_id;
+    productos_id.push(id);
+   }
+   
+
+    for(let i = 0; i < this.restService.articulosS.data.length;i++)
+    {
+      var id = this.restService.articulosS.data[i].id;
+      articulos2.push(id);
+    }
+    console.log(productos_id);
+    console.log(articulos2);
+
+    this.articulos=this.restService.articulosS.data
+    console.log(this.articulos)
+    
+    var listaId = articulos2.filter(e=>!productos_id.includes(e));
+    for(let i =0; i<this.articulos.length; i++){
+      for(let j =0; j<listaId.length; j++){
+        if(listaId[j]==this.articulos[i].id){
+          this.articulosFin.push(this.articulos[i])
+          //console.log(this.articulos[i])
+        }
+      }
+      
+    }
+    console.log(this.articulosFin);
+    //console.log(listaId);
+    
+ }
+}
